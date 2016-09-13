95593cd35df210b502c8b87d0cff5e9b39b0ad0a
miguelperezcolom <miguelperezcolom@gmail.com>
2016-09-13 09:31:10 +0200

    Initial commit

---
+++ model/src/main/java/mateu/erp/App.java
@@ -0,0 +1,13 @@
+package mateu.erp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
