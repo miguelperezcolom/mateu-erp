67866dfc7b87f6449768737bfe38764e8a63cfc9
Miguel Pérez Colom <miguelperezcolom@gmail.com>
2017-12-20 13:48:09 +0100

    some improvements

--- model/src/main/java/io/mateu/erp/model/booking/hotel/Pagina1.java
+++ model/src/main/java/io/mateu/erp/model/booking/hotel/Pagina1.java
@@ -3,6 +3,7 @@ package io.mateu.erp.model.booking.hotel;
 import io.mateu.erp.model.financials.Actor;
 import io.mateu.erp.model.world.State;
 import io.mateu.ui.mdd.server.AbstractServerSideWizardPage;
+import io.mateu.ui.mdd.server.annotations.SameLine;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,13 +23,15 @@ public class Pagina1 extends AbstractServerSideWizardPage {
     private State state;
 
     @NotNull
-    private LocalDate formalizationDate = LocalDate.now();
-
-    @NotNull
     private LocalDate checkin = LocalDate.of(2018, 1, 15);
 
+    @SameLine
     @NotNull
     private LocalDate checkout = LocalDate.of(2018, 1, 23);
+
+    @SameLine
+    @NotNull
+    private LocalDate formalizationDate = LocalDate.now();
 
     @NotNull
     private List<Occupation> occupations = Arrays.asList(new Occupation(1, 2, null));
