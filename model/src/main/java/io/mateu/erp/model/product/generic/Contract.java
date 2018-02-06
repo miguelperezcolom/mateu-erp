3bdf623215cdc6f19031fa5292c35885402af82b
miguelperezcolom <miguelperezcolom@gmail.com>
2018-02-06 09:56:56 +0100

    xx

--- model/src/main/java/io/mateu/erp/model/product/generic/Contract.java
+++ model/src/main/java/io/mateu/erp/model/product/generic/Contract.java
@@ -2,12 +2,12 @@ package io.mateu.erp.model.product.generic;
 
 import io.mateu.erp.model.product.AbstractContract;
 import io.mateu.ui.mdd.server.annotations.QLForCombo;
-import io.mateu.ui.mdd.server.annotations.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +15,7 @@ import java.util.List;
  * Created by miguel on 31/1/17.
  */
 @Entity(name = "GenericContract")
-@Table("GENERICCONTRACT")
+@Table(name = "GENERICCONTRACT")
 @Getter
 @Setter
 @QLForCombo(ql = "select x.id, x.title from io.mateu.erp.model.product.generic.Contract x order by x.title")
--- model/src/main/java/io/mateu/erp/model/product/hotel/contracting/DynamicHotelContract.java
+++ model/src/main/java/io/mateu/erp/model/product/hotel/contracting/DynamicHotelContract.java
@@ -9,6 +9,7 @@ import lombok.Setter;
 import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotNull;
 
 @Entity
