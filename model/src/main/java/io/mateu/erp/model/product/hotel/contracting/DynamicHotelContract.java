fab059278e6544a06ddca745d3a7c6286ae99037
miguelperezcolom <miguelperezcolom@gmail.com>
2018-09-03 22:13:49 +0200

    aa

--- model/src/main/java/io/mateu/erp/model/product/hotel/contracting/DynamicHotelContract.java
+++ model/src/main/java/io/mateu/erp/model/product/hotel/contracting/DynamicHotelContract.java
@@ -1,6 +1,7 @@
 package io.mateu.erp.model.product.hotel.contracting;
 
 import io.mateu.erp.dispo.interfaces.product.IHotelContract;
+import io.mateu.erp.model.config.AppConfig;
 import io.mateu.erp.model.product.AbstractContract;
 import io.mateu.erp.model.product.hotel.HotelContractPhoto;
 import lombok.Getter;
@@ -8,6 +9,7 @@ import lombok.Setter;
 
 import javax.persistence.Convert;
 import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
@@ -39,4 +41,12 @@ public class DynamicHotelContract extends AbstractContract implements IHotelCont
 
         return getParent().getTerms();
     }
+
+
+    @Override
+    public String getXslfo(EntityManager em) {
+        return AppConfig.get(em).getXslfoForHotelContract();
+    }
+
+
 }
